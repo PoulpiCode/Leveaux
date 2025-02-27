657b5f130338d96748ddfa7d23de6fe0540aff3a
Adrien <adrien.godet@etud.u-picardie.fr>
2025-02-27 11:23:25 +0100

    Push pour save

--- Leveaux/app/crea/page.tsx
+++ Leveaux/app/crea/page.tsx
@@ -1,202 +1,100 @@
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
 
-const filters = [
-	{ name: "Industries", values: ["None", "Beauty", "Car dealerships"] },
-	{ name: "Use cases", values: ["None", "HR & Recruiting"] },
-	{ name: "Functions", values: ["None", "Automations", "Integrations"] },
-	{ name: "Company size", values: ["None", "1-10, 11-50", "51-200", "200+"] },
-];
-
-const posts = [
+const people = [
 	{
-		id: "Photo 1",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		href: "#",
-		image: "20250219_132749-min.jpeg",
+		id: "person-1",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 	{
-		id: "post-2",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_132806-min.jpeg",
+		id: "person-2",
+		name: "Name",
+		role: "Role",
+		description: "Elig doloremque mollitia fugiat omnis!",
 	},
 	{
-		id: "post-3",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_132817-min.jpeg",
+		id: "person-3",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 	{
-		id: "post-4",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_132845-min.jpeg",
+		id: "person-4",
+		name: "Name",
+		role: "Role",
+		description: "Elig doloremque mollitia fugiat omnis!",
 	},
 	{
-		id: "post-5",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_132900-min.jpeg",
+		id: "person-5",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 	{
-		id: "post-6",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_132930-min.jpeg",
+		id: "person-6",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 	{
-		id: "Photo 7",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		href: "#",
-		image: "20250219_132956-min.jpeg",
+		id: "person-7",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 	{
-		id: "photo 8",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "20250219_133545-min.jpeg",
-	},
-	{
-		id: "photo 9",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "https://shadcnblocks.com/images/block/placeholder-dark-1.svg",
-	},
-	{
-		id: "photo 10",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "https://shadcnblocks.com/images/block/placeholder-dark-1.svg",
-	},
-	{
-		id: "photo 11",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "https://shadcnblocks.com/images/block/placeholder-dark-1.svg",
-	},
-	{
-		id: "photo 12",
-		title: "Duis sem sem, gravida vel porttitor eu, volutpat ut arcu",
-		summary:
-			"Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.",
-		label: "Ut varius dolor turpis",
-		href: "#",
-		image: "https://shadcnblocks.com/images/block/placeholder-dark-1.svg",
+		id: "person-8",
+		name: "Name",
+		role: "Role",
+		description:
+			"Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
 	},
 ];
 
-const Hero16 = () => {
+const Team5 = () => {
 	return (
-		<section className="">
-			<div className="container flex flex-col items-center text-center ">
-				<h1 className="my-3 text-pretty text-2xl font-bold sm:text-4xl md:my-6 lg:text-5xl py-6">
-					Nos Créations
-				</h1>
-			</div>
-			<div className="container">
-				<div className="aspect-w-16 aspect-h-9 [mask-image:linear-gradient(#000_80%,transparent_100%)]">
-					<img
-						src="Aubade2_Leveaux9.png"
-						alt="placeholder hero"
-						className="h-full w-full rounded-md object-cover"
-					/>
+		<section className="py-32">
+			<div className="container flex flex-col items-center text-center">
+				<p className="semibold">We&apos;re hiring</p>
+				<h2 className="my-6 text-pretty text-2xl font-bold lg:text-4xl">
+					Meet our team
+				</h2>
+				<p className="mb-8 max-w-3xl text-muted-foreground lg:text-xl">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
+					doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur.
+					Explicabo.
+				</p>
+				<div className="flex w-full flex-col justify-center gap-2 sm:flex-row">
+					<Button variant="outline" className="w-full sm:w-auto">
+						Secondary
+					</Button>
+					<Button className="w-full sm:w-auto">Primary</Button>
 				</div>
 			</div>
-			<div className="container">
-				<div className="mb-8 md:mb-10 lg:mb-12">
-					<div className="mb-10 flex flex-wrap items-center gap-x-4 gap-y-3 lg:gap-x-3">
-						{filters.map((filter) => (
-							<div key={filter.name} className="shrink-0 md:w-52 lg:w-56">
-								<Select>
-									<SelectTrigger>
-										<SelectValue placeholder={filter.name} />
-									</SelectTrigger>
-									<SelectContent>
-										{filter.values.map((value) => (
-											<SelectItem key={value} value={value}>
-												{value}
-											</SelectItem>
-										))}
-									</SelectContent>
-								</Select>
-							</div>
-						))}
+			<div className="container mt-16 grid gap-8 md:grid-cols-2 lg:grid-cols-4 lg:px-32">
+				{people.map((person) => (
+					<div key={person.id} className="flex flex-col items-center">
+						<div className="mb-4 aspect-square w-full overflow-clip bg-accent md:mb-5">
+							{/* Avatar */}
+						</div>
+						<p className="w-full text-left font-medium">{person.name}</p>
+						<p className="w-full text-left text-muted-foreground">
+							{person.role}
+						</p>
+						<p className="w-full py-3 text-sm text-muted-foreground">
+							{person.description}
+						</p>
 					</div>
-				</div>
-				<div className="grid gap-4 md:grid-cols-2 lg:gap-6 2xl:grid-cols-3">
-					{posts.map((post) => (
-						<a
-							key={post.id}
-							href={post.href}
-							className="group flex flex-col justify-between rounded-xl border border-border bg-accent p-6"
-						>
-							<div>
-								<div className="flex aspect-[3/2] overflow-clip rounded-xl">
-									<div className="flex-1">
-										<div className="relative h-full w-full origin-bottom transition duration-300 group-hover:scale-105">
-											<img
-												src={post.image}
-												alt={post.title}
-												className="h-full w-full object-cover object-center"
-											/>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="mb-2 line-clamp-3 break-words pt-4 text-lg font-medium md:mb-3 md:pt-4 md:text-2xl lg:pt-4 lg:text-3xl">
-								{post.title}
-							</div>
-							<div className="mb-8 line-clamp-2 text-sm text-muted-foreground md:mb-12 md:text-base lg:mb-9">
-								{post.summary}
-							</div>
-							<div>
-								<Badge>{post.label}</Badge>
-							</div>
-						</a>
-					))}
-				</div>
+				))}
 			</div>
 		</section>
 	);
 };
 
-export default Hero16;
+export default Team5;
